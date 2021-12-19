c3ed6679c7a8c70936d4f3c692fd1bf657680889
BlackDiamond SC Inc [BDSCI TOKEN] <73549208+1Blackdiamondsc@users.noreply.github.com>
2021-12-19 18:59:49 -0500

    Update index.jsx

--- src/components/SecPrices/PricingItem/index.jsx
+++ src/components/SecPrices/PricingItem/index.jsx
@@ -5,8 +5,8 @@ const PricingItem = ({ Round, dollar, bonus }) => {
         <div className="pricing-item ">
           <h4>Round {Round}</h4> 
           <h3><strong className="xzc-1-month">{dollar}</strong></h3> 
-          <span>1 ETH = 500 Token</span>
-          <div className="pricing">15,000,000 Token</div>
+          <span>1 AVAX= 2 Token</span>
+          <div className="pricing">19,000,000 Token</div>
           <label><strong>{bonus} bonus</strong></label>
         </div>
       </div>
