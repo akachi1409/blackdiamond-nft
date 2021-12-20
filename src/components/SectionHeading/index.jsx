9bead74ded41e1f71b98986574a2f927c59ca1e6
BlackDiamond SC Inc [BDSCI TOKEN] <73549208+1Blackdiamondsc@users.noreply.github.com>
2021-12-19 19:03:45 -0500

    Update index.jsx

--- src/components/SectionHeading/index.jsx
+++ src/components/SectionHeading/index.jsx
@@ -22,7 +22,7 @@ const SectionHeading = ({
 	            <span className={ClassSpanTitle}>{title}</span>
 	        </div>
             <h2 data-aos="fade-up">{text}</h2>
-            <p data-aos="fade-up">Our ICO Template Will Be A Perfect Platform For Presenting Your Ico Launch. This Landing Page Comes In Great And Clean Design</p>
+            <p data-aos="fade-up">BlackDiamondSCInc1 Smart Contract Platform & Ico Launch.</p>
 	      </div>
 
       	)}
