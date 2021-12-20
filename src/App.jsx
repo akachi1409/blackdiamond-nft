096ff211612bda67390fb8b01e5419c504667a99
trusteddev1325 <trusteddev1325@gmail.com>
2021-12-21 02:38:12 +0900

    change router

--- src/App.jsx
+++ src/App.jsx
@@ -42,13 +42,13 @@ const App = () => {
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />
         </Helmet>
   			<Switch>
-          <Route path="/" exact component={Home} />
+          <Route path="/" exact component={HomeDemo2} />
   				<Route path="/index-demo-1" component={HomeDemo1} />
-          <Route path="/index-demo-2" component={HomeDemo2} />
+          {/* <Route path="/index-demo-2" component={HomeDemo2} />
           <Route path="/index-demo-3" component={HomeDemo3} />
           <Route path="/index-demo-4" component={HomeDemo4} />
           <Route path="/index-demo-5" component={HomeDemo5} />
-          <Route path="/index-demo-6" component={HomeDemo6} />
+          <Route path="/index-demo-6" component={HomeDemo6} /> */}
   			</Switch>
 	    </div>    
   );
